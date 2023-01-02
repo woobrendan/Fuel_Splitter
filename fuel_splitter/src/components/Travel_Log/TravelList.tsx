f499699fe76a095abd6091d549658e70ea69c804
Brendan Woo <woo.brendan@gmail.com>
2023-01-02 01:20:14 -0500

    create table set up using MUI

--- fuel_splitter/src/components/Travel_Log/TravelList.tsx
+++ fuel_splitter/src/components/Travel_Log/TravelList.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import TravelRow from "./TravelRow";
 
 const TravelList: React.FC = () => {
-  return <div></div>;
+  return (
+    <div id="travelList__container">
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead className="result-table-head">
+            <TableRow>
+              <TableCell>Trip #</TableCell>
+              <TableCell>Total KM's</TableCell>
+              <TableCell>Involved</TableCell>
+            </TableRow>
+          </TableHead>
+          {/* <TravelRow />  LOOP THRU ALL ROWS */}
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default TravelList;
