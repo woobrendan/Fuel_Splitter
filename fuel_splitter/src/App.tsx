9682b6aea0dba0363eb1193fd02174d0afe73545
Brendan Woo <woo.brendan@gmail.com>
2023-01-03 13:40:42 -0500

    remove css import, import NewTrip for testing, add to home route

--- fuel_splitter/src/App.tsx
+++ fuel_splitter/src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import NewTrip from "./components/NewTrip";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path='/' element={} /> */}
+          <Route path="/" element={<NewTrip />} />
           {/* <Route path='/history' element={<History />} /> */}
         </Routes>
       </div>
