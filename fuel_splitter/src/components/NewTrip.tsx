dc5b4642edf4d197543bd636183e5468540aae9e
Brendan Woo <woo.brendan@gmail.com>
2023-01-03 13:54:59 -0500

    change deafult checked for all checkboxes to null. add button for submit. change wrapper div to form passing onsubmit as handleAdd

--- fuel_splitter/src/components/NewTrip.tsx
+++ fuel_splitter/src/components/NewTrip.tsx
@@ -1,18 +1,19 @@
-import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import { FormGroup, FormControlLabel, Checkbox, Button } from "@mui/material";
 
 const NewTrip: React.FC = () => {
   return (
-    <div className="newTrip__container">
+    <form className="newTrip__container" onSubmit={(e) = > {handleAdd(e)}}>
+      <h1>Add New Trip Info</h1>
       <FormGroup className="newTrip__checkboxes">
-        <FormControlLabel
-          control={<Checkbox defaultChecked />}
-          label="Brendan"
-        />
-        <FormControlLabel control={<Checkbox defaultChecked />} label="Lory" />
-        <FormControlLabel control={<Checkbox defaultChecked />} label="David" />
-        <FormControlLabel control={<Checkbox defaultChecked />} label="Parco" />
+        <FormControlLabel control={<Checkbox />} label="Brendan" />
+        <FormControlLabel control={<Checkbox />} label="Lory" />
+        <FormControlLabel control={<Checkbox />} label="David" />
+        <FormControlLabel control={<Checkbox />} label="Parco" />
       </FormGroup>
-    </div>
+      <Button variant="contained" color="success">
+        Submit
+      </Button>
+    </form>
   );
 };
 
