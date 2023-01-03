9bf91270711d9d9b0300edd3895bbea94c6303e9
Brendan Woo <woo.brendan@gmail.com>
2023-01-03 14:03:58 -0500

    create interface Props passings method handleAdd. pass Props type to React.FC

--- fuel_splitter/src/components/NewTrip.tsx
+++ fuel_splitter/src/components/NewTrip.tsx
@@ -1,8 +1,17 @@
 import { FormGroup, FormControlLabel, Checkbox, Button } from "@mui/material";
 
-const NewTrip: React.FC = () => {
+interface Props {
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const NewTrip: React.FC<Props> = ({ handleAdd }) => {
   return (
-    <form className="newTrip__container" onSubmit={(e) = > {handleAdd(e)}}>
+    <form
+      className="newTrip__container"
+      onSubmit={(e) => {
+        handleAdd(e);
+      }}
+    >
       <h1>Add New Trip Info</h1>
       <FormGroup className="newTrip__checkboxes">
         <FormControlLabel control={<Checkbox />} label="Brendan" />
