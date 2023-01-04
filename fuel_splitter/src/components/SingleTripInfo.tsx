2b136d1bc6d9d9068d4078ed816424fc7e86ff87
Brendan Woo <woo.brendan@gmail.com>
2023-01-04 15:55:07 -0500

    add to handleAdd method. dispatch fuelBillActions.addNewTrip passing trip parameter. use appSelector to get fuelBill State

--- fuel_splitter/src/components/SingleTripInfo.tsx
+++ fuel_splitter/src/components/SingleTripInfo.tsx
@@ -1,13 +1,19 @@
 import NewTrip from "./NewTrip";
 import { useAppSelector, useAppDispatch } from "../store/hooks";
+import "../Styles/tripinfo.scss";
+import { TripInfo } from "../model";
+import { fuelBillActions } from "../store/GasTripSlice";
 
 const SingleTripInfo: React.FC = () => {
   const dispatch = useAppDispatch();
-  const handleAdd = (e: React.FormEvent) => {
+  const handleAdd = (e: React.FormEvent, trip: TripInfo) => {
     e.preventDefault();
     //dispatch gas trip action
-    console.log("Added");
+    dispatch(fuelBillActions.addNewTrip(trip));
   };
+
+  const oneBill = useAppSelector((state) => state.fuelBill);
+  console.log("oneTrip", oneBill);
 
   return (
     <>
