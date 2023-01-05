b51c315567137e1a52a561da48876ed353df9344
Brendan Woo <woo.brendan@gmail.com>
2023-01-05 17:25:35 -0500

    change fragment wrapper to ev id. import TravelList and render with NewTrip

--- fuel_splitter/src/components/SingleTripInfo.tsx
+++ fuel_splitter/src/components/SingleTripInfo.tsx
@@ -3,6 +3,7 @@ import { useAppSelector, useAppDispatch } from "../store/hooks";
 import "../Styles/tripinfo.scss";
 import { TripInfo } from "../model";
 import { fuelBillActions } from "../store/GasTripSlice";
+import TravelList from "./Travel_Log/TravelList";
 
 const SingleTripInfo: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -16,9 +17,10 @@ const SingleTripInfo: React.FC = () => {
   console.log("oneTrip", oneBill);
 
   return (
-    <>
+    <div id="single_trip__container">
+      <TravelList />
       <NewTrip handleAdd={handleAdd} />
-    </>
+    </div>
   );
 };
 
