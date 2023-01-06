78d485f6501076403a7f9004a8908ab2870dc4f2
Brendan Woo <woo.brendan@gmail.com>
2023-01-06 15:10:54 -0500

    create component for getting final gas payment and submitting all

---
+++ fuel_splitter/src/components/GasPay.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const GasPay: React.FC = () => {
+  return (
+    <form className="single_trip__submitAll">
+      <Button variant="contained" color="success" type="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default GasPay;
