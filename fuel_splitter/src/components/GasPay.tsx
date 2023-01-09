7f1c209ba0af8c156fe31670c455606caa628dbc
Brendan Woo <woo.brendan@gmail.com>
2023-01-09 16:16:31 -0500

    create interface GasBill, pass to usestate and reshape as gasBill with object gasCost and costPerL. adjust onchange to handle new event and set to object. copy div and create new input field for costPerL

--- fuel_splitter/src/components/GasPay.tsx
+++ fuel_splitter/src/components/GasPay.tsx
@@ -1,18 +1,39 @@
 import { Button } from "@mui/material";
 import { useState } from "react";
 
+interface GasBill {
+  gasCost: number;
+  costPerL: number;
+}
+
 const GasPay: React.FC = () => {
-  const [gasCost, setGasCost] = useState<number>(0);
+  const [gasBill, setGasBill] = useState<GasBill>({
+    gasCost: 0,
+    costPerL: 0,
+  });
 
   return (
     <form className="single_trip__submitAll">
       <div className="single_trip__submitAll__input">
+        <label>Cost per Litre:</label>
+        <input
+          type="input"
+          value={gasBill.costPerL}
+          // onChange={(e) => setGasCost(Number(e.target.value))}
+        />
+      </div>
+      <div className="single_trip__submitAll__input">
         <label>Total Gas Paid:</label>
         <input
-          placeholder="Enter Total KM's travelled"
           type="input"
-          value={gasCost}
-          onChange={(e) => setGasCost(Number(e.target.value))}
+          value={gasBill.gasCost}
+          onChange={(e: React.FormEvent) => {
+            const target = e.target as HTMLTextAreaElement;
+            setGasBill((prev) => ({
+              ...prev,
+              gasCost: Number(target.value),
+            }));
+          }}
         />
       </div>
       <Button variant="contained" color="success" type="submit">
