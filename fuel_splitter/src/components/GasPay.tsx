399a7970f1f03921f12a72571ad2a1796235b988
Brendan Woo <woo.brendan@gmail.com>
2023-01-09 16:03:48 -0500

    create usestate gasCost pass value to input and set onChange for setGasCost

--- fuel_splitter/src/components/GasPay.tsx
+++ fuel_splitter/src/components/GasPay.tsx
@@ -1,10 +1,22 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
 
 const GasPay: React.FC = () => {
+  const [gasCost, setGasCost] = useState<number>(0);
+
   return (
     <form className="single_trip__submitAll">
+      <div className="single_trip__submitAll__input">
+        <label>Total Gas Paid:</label>
+        <input
+          placeholder="Enter Total KM's travelled"
+          type="input"
+          value={gasCost}
+          onChange={(e) => setGasCost(Number(e.target.value))}
+        />
+      </div>
       <Button variant="contained" color="success" type="submit">
-        Submit
+        Submit All
       </Button>
     </form>
   );
