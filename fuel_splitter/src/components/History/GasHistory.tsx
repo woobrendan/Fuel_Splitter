1d711ef9ee275e8528fb559b452a931bb9965adc
Brendan Woo <woo.brendan@gmail.com>
2023-01-12 15:52:35 -0500

    remove tempHistory import, change render back to history from store

--- fuel_splitter/src/components/History/GasHistory.tsx
+++ fuel_splitter/src/components/History/GasHistory.tsx
@@ -1,14 +1,13 @@
 import SingleHistory from "./SingleHistory";
 import { useAppSelector } from "../../store/hooks";
 import "../../Styles/history.scss";
-import { tempHistory } from "../../temp";
 
 const GasHistory: React.FC = () => {
   const history = useAppSelector((state) => state.fuelHistory.history);
 
   return (
     <section id="history">
-      {tempHistory
+      {history
         .map((bill, index) => (
           <SingleHistory bill={bill} key={index} tripNum={index + 1} />
         ))
