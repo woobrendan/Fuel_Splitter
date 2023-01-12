09434442d087d98606bc14a50d31e8098adc4a23
Brendan Woo <woo.brendan@gmail.com>
2023-01-12 15:53:18 -0500

    import tempHistory and add to initialState

--- fuel_splitter/src/store/historySlice.tsx
+++ fuel_splitter/src/store/historySlice.tsx
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { FuelBill } from "../model";
+import { tempHistory } from "../temp";
 
 interface HistoryState {
   history: FuelBill[];
 }
 
-const initialState: HistoryState = { history: [] };
+const initialState: HistoryState = { history: [...tempHistory] };
 
 const historySlice = createSlice({
   name: "history",
