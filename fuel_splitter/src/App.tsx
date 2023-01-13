bc7cfcffc7a4d840da452115d69f9a60ad1242b3
Brendan Woo <woo.brendan@gmail.com>
2023-01-13 16:48:44 -0500

    import and render Nav Bar at top of component

--- fuel_splitter/src/App.tsx
+++ fuel_splitter/src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import SingleTripInfo from "./components/SingleTripInfo";
 import GasHistory from "./components/History/GasHistory";
 import "./Styles/app.scss";
+import NavBar from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <NavBar />
       <div className="App">
         <Routes>
           <Route path="/" element={<SingleTripInfo />} />
