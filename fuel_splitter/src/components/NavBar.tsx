624130bca4d0113980c62aefa9aef44508b86564
Brendan Woo <woo.brendan@gmail.com>
2023-01-13 16:46:11 -0500

    add h1 title to navbar, update import for style sheet

--- fuel_splitter/src/components/NavBar.tsx
+++ fuel_splitter/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Toolbar, AppBar } from "@mui/material";
-import "../Styling/navBar.scss";
+import "../Styles/navBar.scss";
 import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
@@ -10,6 +10,7 @@ const NavBar: React.FC = () => {
         <AppBar position="static" sx={{ bgcolor: "#FF0000" }}>
           <Toolbar>
             <div className="navbar-links">
+              <h1>Fuel Splitter</h1>
               <Link to="/">Trip Management</Link>
               <Link to="/history">History</Link>
             </div>
