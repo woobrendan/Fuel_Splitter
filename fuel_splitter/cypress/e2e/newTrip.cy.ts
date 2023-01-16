95ea17211a8af054e39fed2665d91cca0f36cd60
Brendan Woo <woo.brendan@gmail.com>
2023-01-16 12:24:44 -0800

    create test to check for newTrip container, and that there are no checks. then test if checks work

--- fuel_splitter/cypress/e2e/newTrip.cy.ts
+++ fuel_splitter/cypress/e2e/newTrip.cy.ts
@@ -12,4 +12,30 @@ describe("Adding New Trip", () => {
       ".MuiTableBody-root > .MuiTableRow-root > th.MuiTableCell-root",
     ).should("not.exist");
   });
+
+  it("Should have container to add new trip", () => {
+    cy.get(".newTrip__container");
+  });
+
+  it("New Trip should not have any checks", () => {
+    cy.get(
+      ":nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input",
+    ).should("not.be.checked");
+    cy.get(
+      ":nth-child(2) > .MuiButtonBase-root > .PrivateSwitchBase-input",
+    ).should("not.be.checked");
+    cy.get(
+      ":nth-child(3) > .MuiButtonBase-root > .PrivateSwitchBase-input",
+    ).should("not.be.checked");
+    cy.get(
+      ":nth-child(4) > .MuiButtonBase-root > .PrivateSwitchBase-input",
+    ).should("not.be.checked");
+  });
+
+  it("Should be able to add trip participants", () => {
+    cy.get(".MuiFormGroup-root > :nth-child(1)").click();
+    cy.get(
+      ":nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input",
+    ).should("be.checked");
+  });
 });
