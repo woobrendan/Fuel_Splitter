8adf997234877e309df7954bfc181a30ef670276
Brendan Woo <woo.brendan@gmail.com>
2023-01-16 12:16:00 -0800

    set up cypress test for new trip

--- fuel_splitter/cypress/e2e/newTrip.cy.ts
+++ fuel_splitter/cypress/e2e/newTrip.cy.ts
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Adding New Trip", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Should have a table", () => {
+    cy.get(".css-1ygcj2i-MuiTableCell-root");
+  });
+
+  it("Should have a table without rows", () => {
+    cy.get(
+      ".MuiTableBody-root > .MuiTableRow-root > th.MuiTableCell-root",
+    ).should("not.exist");
+  });
+});
