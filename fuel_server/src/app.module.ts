d3bf12ed1eea6529477c9cf6b5d136928e242121
Brendan Woo <woo.brendan@gmail.com>
2023-01-20 13:41:04 -0800

    import mongoosemodule and add to imports with forRoot to mongo url

--- fuel_server/src/app.module.ts
+++ fuel_server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { BillModule } from './bill/bill.module';
 import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [AuthModule, BillModule, HistoryModule],
+  imports: [
+    AuthModule,
+    BillModule,
+    HistoryModule,
+    MongooseModule.forRoot(process.env.COMPASS_URL),
+  ],
 })
 export class AppModule {}
