ff120ac0ed60b78f6fb2dd4f03d781def0468b7a
Brendan Woo <woo.brendan@gmail.com>
2023-01-20 11:43:49 -0800

    import billSchema and add imports to module for mongoosemodule

--- fuel_server/src/bill/bill.module.ts
+++ fuel_server/src/bill/bill.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BillController } from './bill.controller';
 import { BillService } from './bill.service';
+import { BillSchema, Bill } from 'schemas/bill.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Bill', schema: BillSchema }])],
   controllers: [BillController],
   providers: [BillService],
 })
