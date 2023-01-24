b7281f3c333aa9604f50fcfe691da9f0f363b15f
Brendan Woo <woo.brendan@gmail.com>
2023-01-24 14:40:51 -0800

    create component that houses just the individual breakdown of a bill

---
+++ fuel_splitter/src/components/History/IndivBreakDown.tsx
@@ -0,0 +1,55 @@
+import {
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TableBody,
+} from "@mui/material";
+import { FuelBill } from "../../model";
+
+interface Props {
+  bill: FuelBill;
+}
+
+const TripDetails: React.FC<Props> = ({ bill }) => {
+  const { brendan, lory, david, parco } = bill;
+  const tripInfo = [brendan, lory, david, parco];
+
+  return (
+    <div className="history__single__tripDetails">
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">KM's</TableCell>
+              <TableCell align="right"># of Trips</TableCell>
+              <TableCell align="right">Bill %</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tripInfo.map((trip, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row" align="left">
+                  {trip.name}
+                </TableCell>
+                <TableCell align="right">{trip.totalKM}</TableCell>
+                <TableCell align="right">{trip.totalTrips}</TableCell>
+                <TableCell align="right">
+                  {(trip.billPortion * 100).toFixed()}%
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default TripDetails;
