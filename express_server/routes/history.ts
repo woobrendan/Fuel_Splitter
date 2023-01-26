37ed8cbb3138e6fa256c58ccdef493083dcd6058
Brendan Woo <woo.brendan@gmail.com>
2023-01-26 13:44:35 -0800

    change express import from require to import. import request and response types from express and add to req and res for types

--- express_server/routes/history.ts
+++ express_server/routes/history.ts
@@ -1,12 +1,13 @@
-const express = require("express");
+// const express = require("express");
+import express, { Request, Response } from "express";
 const router = express.Router();
 const history = require("../models/history_schema");
 
-router.get("/", async (req: Express.Request, res: Express.Response) => {
+router.get("/", async (req: Request, res: Response) => {
   try {
     const results = await history.find();
     res.json(results);
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).json({ message: err.message });
   }
 });
