5206c9c52ffb18bb32a67963c7d514f0afc5bbe2
Brendan Woo <woo.brendan@gmail.com>
2023-01-26 12:38:31 -0800

    create models file to house schemas, create fuelBill schema

---
+++ express_server/models/fuelBill_schema.ts
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const fuelBillSchema = new mongoose.Schema({
+  totalPrice: { type: Number },
+  totalKM: { type: Number },
+  costPerLitre: { type: Number },
+  brendan: {
+    name: { type: String },
+    totalKM: { type: Number },
+    totalTrips: { type: Number },
+    billPortion: { type: Number },
+  },
+  lory: {
+    name: { type: String },
+    totalKM: { type: Number },
+    totalTrips: { type: Number },
+    billPortion: { type: Number },
+  },
+  david: {
+    name: { type: String },
+    totalKM: { type: Number },
+    totalTrips: { type: Number },
+    billPortion: { type: Number },
+  },
+  parco: {
+    name: { type: String },
+    totalKM: { type: Number },
+    totalTrips: { type: Number },
+    billPortion: { type: Number },
+  },
+  tripLogs: { type: Array },
+});
+
+module.exports = mongoose.model("fuelBill", fuelBillSchema);
