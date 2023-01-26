e32db1ecf74d600d8773a90f60073e5131a61ab8
Brendan Woo <woo.brendan@gmail.com>
2023-01-26 13:04:02 -0800

    setup basic router middleware, exporting router and having basic get request

--- express_server/routes/fuelBill.ts
+++ express_server/routes/fuelBill.ts
@@ -1,6 +1,8 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  res.send("Hello Me World");
+});
 
-export default router;
+module.exports = router;
