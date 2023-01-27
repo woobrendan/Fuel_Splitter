2615e143319f99a526a227b87cf2c8ebe895d818
Brendan Woo <woo.brendan@gmail.com>
2023-01-27 15:18:08 -0800

    import model file from fuel splitter to use for schemas

---
+++ express_server/models/model.ts
@@ -0,0 +1,32 @@
+export interface IndividualInfo {
+  name?: string;
+  totalKM: number;
+  totalTrips: number;
+  billPortion: number;
+}
+
+export interface FuelBill {
+  totalPrice: number;
+  totalKM: number;
+  costPerLitre: number;
+  brendan: IndividualInfo;
+  lory: IndividualInfo;
+  david: IndividualInfo;
+  parco: IndividualInfo;
+  tripLogs: TripInfo[];
+}
+
+export interface TripInfo {
+  isBrendanIn: boolean;
+  isLoryIn: boolean;
+  isDavidIn: boolean;
+  isParcoIn: boolean;
+  totalKM: number;
+  date: Date | null;
+  description: string;
+}
+
+export interface GasBill {
+  gasCost: number;
+  costPerL: number;
+}
