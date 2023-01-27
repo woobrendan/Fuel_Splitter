8f74653793bcc4861c6021b2fb8fb430376c3fce
Brendan Woo <woo.brendan@gmail.com>
2023-01-27 15:19:25 -0800

    add in fuelBill route to get from mongo fueBill collection

--- express_server/routes/fuelBill.ts
+++ express_server/routes/fuelBill.ts
@@ -1,8 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const router = express.Router();
+const fuelBill = require("../models/fuelBill_schema");
 
-router.get("/", (req, res) => {
-  res.send("Hello Me World");
+// router.get("/", (req, res) => {
+//   res.send("Hello Me World");
+// });
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const results = await fuelBill.find();
+    console.log("results", results);
+    res.json(results);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
