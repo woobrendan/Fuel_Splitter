8749bb588263065bac4e7e9f78e0bccd03ace855
Brendan Woo <woo.brendan@gmail.com>
2023-01-27 16:30:56 -0800

    complete logic for creating history, read, and readall

--- express_server/controllers/history_controller.ts
+++ express_server/controllers/history_controller.ts
@@ -16,8 +16,33 @@ const createHistory = (req: Request, res: Response, next: NextFunction) => {
     parco: { ...req.body.parco },
     tripLogs: copyOfArr,
   });
+
+  return history
+    .save()
+    .then((history) => res.status(201).json({ history }))
+    .catch((error) => res.status(500).json({ error }));
+
+  // await history.save()
+  // res.status(201).json({history})
 };
-const readHistory = (req: Request, res: Response, next: NextFunction) => {};
-const readAllHistory = (req: Request, res: Response, next: NextFunction) => {};
+
+const readHistory = (req: Request, res: Response, next: NextFunction) => {
+  const historyId = req.params.historyId;
+  return History.findById(historyId)
+    .then((history) =>
+      history
+        ? res.status(200).json({ history })
+        : res.status(404).json({ message: "History not found" }),
+    )
+    .catch((error) => res.status(500).json({ error }));
+};
+
+const readAll = (req: Request, res: Response, next: NextFunction) => {
+  return History.find()
+    .then((history) => res.status(200).json({ history }))
+    .catch((error) => res.status(500).json({ error }));
+};
+
 const updateHistory = (req: Request, res: Response, next: NextFunction) => {};
+
 const deleteHistory = (req: Request, res: Response, next: NextFunction) => {};
