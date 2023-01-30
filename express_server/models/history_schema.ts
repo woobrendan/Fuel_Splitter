d0d4b5a07aaac0c4e36aeb1057fa3c3f57d107cd
Brendan Woo <woo.brendan@gmail.com>
2023-01-30 10:36:25 -0800

    import Document Document and SChema from mongoose. export new FuelBillModel form FuelBill and docuemnt. add new option for version key to be false. export default mode type fuelBillModel

--- express_server/models/history_schema.ts
+++ express_server/models/history_schema.ts
@@ -1,34 +1,44 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+import mongoose, { Document, Schema } from "mongoose";
+import { FuelBill } from "../models/model";
 
-const historySchema = new mongoose.Schema({
-  totalPrice: { type: Number, required: true },
-  totalKM: { type: Number, required: true },
-  costPerLitre: { type: Number, required: true },
-  brendan: {
-    name: { type: String },
-    totalKM: { type: Number },
-    totalTrips: { type: Number },
-    billPortion: { type: Number },
-  },
-  lory: {
-    name: { type: String },
-    totalKM: { type: Number },
-    totalTrips: { type: Number },
-    billPortion: { type: Number },
-  },
-  david: {
-    name: { type: String },
-    totalKM: { type: Number },
-    totalTrips: { type: Number },
-    billPortion: { type: Number },
-  },
-  parco: {
-    name: { type: String },
-    totalKM: { type: Number },
-    totalTrips: { type: Number },
-    billPortion: { type: Number },
-  },
-  tripLogs: { type: Array },
-});
+export interface FuelBillModel extends FuelBill, Document {}
 
-module.exports = mongoose.model("history", historySchema);
+const historySchema: Schema = new Schema(
+  {
+    _id: String,
+    totalPrice: { type: Number, required: true },
+    totalKM: { type: Number, required: true },
+    costPerLitre: { type: Number, required: true },
+    brendan: {
+      name: { type: String },
+      totalKM: { type: Number },
+      totalTrips: { type: Number },
+      billPortion: { type: Number },
+    },
+    lory: {
+      name: { type: String },
+      totalKM: { type: Number },
+      totalTrips: { type: Number },
+      billPortion: { type: Number },
+    },
+    david: {
+      name: { type: String },
+      totalKM: { type: Number },
+      totalTrips: { type: Number },
+      billPortion: { type: Number },
+    },
+    parco: {
+      name: { type: String },
+      totalKM: { type: Number },
+      totalTrips: { type: Number },
+      billPortion: { type: Number },
+    },
+    tripLogs: { type: Array },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+export default mongoose.model<FuelBillModel>("History", historySchema);
