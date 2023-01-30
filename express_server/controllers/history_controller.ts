5fa276d7cc6cbf1be7387aa5575e23032414b3f6
Brendan Woo <woo.brendan@gmail.com>
2023-01-30 10:47:46 -0800

    import TripInfo interface and apply to log for map of tripLogs. change read by Id to use try catch instead of then catch. create logic for updtae and delete history. export default object fall controllers

--- express_server/controllers/history_controller.ts
+++ express_server/controllers/history_controller.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import History from "../models/history_schema";
+import { TripInfo } from "../models/model";
 
 const createHistory = (req: Request, res: Response, next: NextFunction) => {
   const { totalKM, totalPrice, costPerLitre } = req.body;
-  const copyOfArr = req.body.tripLogs.map((log) => ({ ...log }));
+  const copyOfArr = req.body.tripLogs.map((log: TripInfo) => ({ ...log }));
   const history = new History({
     _id: new mongoose.Types.ObjectId(),
     totalKM,
@@ -26,15 +27,16 @@ const createHistory = (req: Request, res: Response, next: NextFunction) => {
   // res.status(201).json({history})
 };
 
-const readHistory = (req: Request, res: Response, next: NextFunction) => {
+const readHistory = async (req: Request, res: Response) => {
   const historyId = req.params.historyId;
-  return History.findById(historyId)
-    .then((history) =>
-      history
-        ? res.status(200).json({ history })
-        : res.status(404).json({ message: "History not found" }),
-    )
-    .catch((error) => res.status(500).json({ error }));
+  try {
+    const history = await History.findById(historyId);
+    return history
+      ? res.status(200).json({ history })
+      : res.status(404).json({ message: "History not found" });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 const readAll = (req: Request, res: Response, next: NextFunction) => {
@@ -43,6 +45,38 @@ const readAll = (req: Request, res: Response, next: NextFunction) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-const updateHistory = (req: Request, res: Response, next: NextFunction) => {};
+const updateHistory = (req: Request, res: Response, next: NextFunction) => {
+  const historyId = req.params.historyId;
+  return History.findById(historyId)
+    .then((history) => {
+      if (history) {
+        history.set(req.body);
+        return history
+          .save()
+          .then((history) => res.status(201).json({ history }))
+          .catch((error) => res.status(500).json({ error }));
+      } else {
+        res.status(404).json({ message: "History not found" });
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
-const deleteHistory = (req: Request, res: Response, next: NextFunction) => {};
+const deleteHistory = (req: Request, res: Response, next: NextFunction) => {
+  const historyId = req.params.historyId;
+  return History.findByIdAndDelete(historyId)
+    .then((history) =>
+      history
+        ? res.status(201).json({ message: "Deleted" })
+        : res.status(404).json({ message: "Not Found" }),
+    )
+    .catch((error) => res.status(500).json({ error }));
+};
+
+export default {
+  createHistory,
+  readHistory,
+  readAll,
+  updateHistory,
+  deleteHistory,
+};
