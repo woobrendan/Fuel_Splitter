3c9cddaf7fb8a9d04aeb7f2fedd9e30563e9dee2
Brendan Woo <woo.brendan@gmail.com>
2023-01-31 11:34:59 -0800

    pass middleware to store, to set serializeable check to false

--- fuel_splitter/src/store/store.ts
+++ fuel_splitter/src/store/store.ts
@@ -7,6 +7,10 @@ const store = configureStore({
     fuelHistory: historySlice.reducer,
     fuelBill: fuelBill.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
