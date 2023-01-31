a2687f0637c3fe4beaf905047117d2e8a167374e
Brendan Woo <woo.brendan@gmail.com>
2023-01-31 12:33:24 -0800

    create state usestate for historyTrips, create useEffect to use new method getHistory. get history array and set to hitosryTips and change map in component to loop through historyTrips

--- fuel_splitter/src/components/History/GasHistory.tsx
+++ fuel_splitter/src/components/History/GasHistory.tsx
@@ -1,13 +1,35 @@
 import SingleHistory from "./SingleHistory";
-import { useAppSelector } from "../../store/hooks";
+// import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import "../../Styles/history.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { FuelBill } from "../../model";
+// import { historyActions } from "../../store/historySlice";
 
 const GasHistory: React.FC = () => {
-  const history = useAppSelector((state) => state.fuelHistory.history);
+  // const history = useAppSelector((state) => state.fuelHistory.history);
+  const [historyTrips, setHistoryTrips] = useState<FuelBill[]>([]);
+
+  // const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getHistory();
+  }, []);
+
+  const getHistory = async () => {
+    try {
+      const data = await axios.get("http://localhost:1212/history/get/");
+      const trips: FuelBill[] = data.data.tripLogs;
+      // dispatch(historyActions.setHistory(trips));
+      setHistoryTrips(() => [...trips]);
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  };
 
   return (
     <section id="history">
-      {history
+      {historyTrips
         .map((bill, index) => (
           <SingleHistory bill={bill} key={index} tripNum={index + 1} />
         ))
