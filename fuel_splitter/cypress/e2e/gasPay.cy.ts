91f6f5200779cbc7a662529e0640900320c1b4a7
Brendan Woo <woo.brendan@gmail.com>
2023-02-16 11:06:18 -0800

    add new test for checking all 3 errors exist

--- fuel_splitter/cypress/e2e/gasPay.cy.ts
+++ fuel_splitter/cypress/e2e/gasPay.cy.ts
@@ -7,8 +7,19 @@ describe("Final Gas Totals", () => {
     cy.visit("http://localhost:3000/");
   });
 
+  it("Should have 3 errors without info", () => {
+    cy.get('[data-cy="submit_all"]').click();
+
+    cy.get('[data-cy="no_trips"]').should("exist");
+    cy.get('[data-cy="gas_paid_error"]').should("exist");
+    cy.get('[data-cy="cost_L_error"]').should("exist");
+  });
+
   it("Should not allow you to submit without any trips", () => {
-    cy.get('[data-cy="cost_per_L"]').clear().type(costPerL);
-    cy.get('[data-cy="total_cost"]').clear().type(totalGas);
+    // cy.get('[data-cy="cost_per_L"]').clear().type(costPerL);
+    // cy.get('[data-cy="total_cost"]').clear().type(totalGas);
+    cy.get('[data-cy="submit_all"]').click();
+
+    cy.get('[data-cy="no_trips"]').should("exist");
   });
 });
