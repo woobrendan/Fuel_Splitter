515159884b980dbac9cbce03b230a9ca984c41a8
Brendan Woo <woo.brendan@gmail.com>
2023-02-17 10:58:40 -0800

    move logic for interface Trip, array trips and method checkEachName to model_func file

--- fuel_splitter/cypress/e2e/newTrip.cy.ts
+++ fuel_splitter/cypress/e2e/newTrip.cy.ts
@@ -1,29 +1,5 @@
 import history from "../seeds/history";
-
-interface Trip {
-  km: string;
-  description: string;
-  names: string[];
-}
-
-const trips: Trip[] = [
-  { km: "25", description: "Airport", names: ["David"] },
-  {
-    km: "14",
-    description: "Gym",
-    names: ["Lory", "Brendan", "Parco", "David"],
-  },
-  { km: "9", description: "Physio", names: ["David", "Lory"] },
-];
-
-const checkEachName = (trip: Trip) => {
-  trip.names.forEach((name) => {
-    cy.get(`[data-testid="checkbox_${name}"]`).click(),
-      cy
-        .get(`[data-testid="checkbox_${name}"] > .PrivateSwitchBase-input`)
-        .should("be.checked");
-  });
-};
+import { Trip, trips, checkEachName } from "../helpers/model_functions";
 
 describe("Adding New Trip", () => {
   beforeEach(() => {
