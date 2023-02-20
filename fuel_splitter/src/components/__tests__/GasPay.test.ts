c90815d25d1d400e45947482571b8aea771100b5
Brendan Woo <woo.brendan@gmail.com>
2023-02-20 12:13:57 -0800

    delete packages from parent folder and install inside fuell splitter folder

--- fuel_splitter/src/components/__tests__/GasPay.test.ts
+++ fuel_splitter/src/components/__tests__/GasPay.test.ts
@@ -1,4 +1,6 @@
 import GasPay from "../TripManage/GasPay";
 import { render, cleanup, fireEvent } from "@testing-library/react";
+// import { afterEach } from "node:test";
+import React from "react";
 
 afterEach(cleanup);
