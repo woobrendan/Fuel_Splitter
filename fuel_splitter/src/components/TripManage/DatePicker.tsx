c1dbd5a9c18e9cf873f3fe1a2cc05899877fa24d
Brendan Woo <woo.brendan@gmail.com>
2023-02-20 11:14:41 -0800

    move datepicker component inside trip management folder

---
+++ fuel_splitter/src/components/TripManage/DatePicker.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Stack, TextField } from "@mui/material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
+interface Props {
+  getDate: (dateVal: Date | null) => void;
+}
+
+const DatePicker: React.FC<Props> = ({ getDate }) => {
+  const [date, setDate] = useState<Date | null>(new Date());
+
+  const handleChange = (newValue: Date | null) => {
+    setDate(newValue);
+    getDate(newValue);
+  };
+
+  return (
+    <div id="date-picker">
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={3}>
+          <DesktopDatePicker
+            label="Date"
+            inputFormat="yyyy-MM-dd"
+            value={date}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
+    </div>
+  );
+};
+
+export default DatePicker;
