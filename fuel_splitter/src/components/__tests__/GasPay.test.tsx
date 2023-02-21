e51c14bd63f88dce1a0eeacc6bc03bfe627a7851
Brendan Woo <woo.brendan@gmail.com>
2023-02-21 11:47:37 -0800

    add testSubmit method to pass to gasPay. create test to test if button clicks

--- fuel_splitter/src/components/__tests__/GasPay.test.tsx
+++ fuel_splitter/src/components/__tests__/GasPay.test.tsx
@@ -1,10 +1,69 @@
 import GasPay from "../TripManage/GasPay";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import React from "react";
+import { GasBill, FuelBill } from "../../model";
+
+const testSubmit = (gasBill: GasBill) => {
+  const copy: FuelBill = {
+    totalKM: 24,
+    brendan: { name: "Brendan", totalKM: 10, totalTrips: 1, billPortion: 0.4 },
+    lory: {
+      name: "Lory",
+      totalKM: 10,
+      totalTrips: 1,
+      billPortion: 0.4,
+    },
+    david: { name: "David", totalKM: 2.5, totalTrips: 1, billPortion: 0.1 },
+    parco: { name: "Parco", totalKM: 2.5, totalTrips: 1, billPortion: 0.1 },
+    tripLogs: [
+      {
+        isBrendanIn: true,
+        isLoryIn: true,
+        isDavidIn: false,
+        isParcoIn: false,
+        totalKM: 20,
+        date: new Date(),
+        description: "Groceries",
+      },
+      {
+        isBrendanIn: false,
+        isLoryIn: false,
+        isDavidIn: true,
+        isParcoIn: true,
+        totalKM: 14,
+        date: new Date(),
+        description: "Gym",
+      },
+    ],
+    totalPrice: gasBill.gasCost,
+    costPerLitre: gasBill.costPerL,
+  };
+
+  return copy;
+  // dispatch(historyActions.addToHistory(copy));
+  // addToHistory(copy);
+
+  // //** reset trip Logs */
+  // dispatch(fuelBillActions.resetGasTrip());
+  // resetTripLog();
+
+  // navigate("/history");
+};
 
 describe(GasPay, () => {
-  it("renders without crashing", () => {
-    render(<GasPay finalSubmit={"test"} />);
-    expect(screen.getByText("Cost Per L:")).toBeInTheDocument;
+  // it("renders without crashing", () => {
+  //   render(<GasPay finalSubmit={testSubmit} />);
+  //   expect(getByText("Cost Per L:")).toBeInTheDocument;
+  // });
+
+  it("can click submit all button", () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(<GasPay finalSubmit={testSubmit} />);
+
+    const button = getByText("Submit All");
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
