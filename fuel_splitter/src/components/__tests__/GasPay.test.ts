9ed2e785b4867b3335f899597822ae26a1975c65
Brendan Woo <woo.brendan@gmail.com>
2023-02-21 10:25:15 -0800

    change file type from typscript to javascript

--- fuel_splitter/src/components/__tests__/GasPay.test.ts
+++
@@ -1,6 +0,0 @@
-import GasPay from "../TripManage/GasPay";
-import { render, cleanup, fireEvent } from "@testing-library/react";
-// import { afterEach } from "node:test";
-import React from "react";
-
-afterEach(cleanup);
