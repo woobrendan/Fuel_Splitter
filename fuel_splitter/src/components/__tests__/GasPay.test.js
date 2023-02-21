44ebedf44efde666dece87cef744c31e5b3301b7
Brendan Woo <woo.brendan@gmail.com>
2023-02-21 11:11:51 -0800

    change file back to tsx file instead of js, since config file is looking for tsx files

--- fuel_splitter/src/components/__tests__/GasPay.test.js
+++
@@ -1,9 +0,0 @@
-import GasPay from "../TripManage/GasPay";
-import { render, cleanup, fireEvent } from "@testing-library/react";
-import React from "react";
-
-afterEach(cleanup);
-
-it("renders without crashing", () => {
-  render(<GasPay finalSubmit={"test"} />);
-});
