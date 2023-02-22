c02949058c54eef9c02a5036e3e2f1f137e76267
Brendan Woo <woo.brendan@gmail.com>
2023-02-22 10:27:08 -0800

    change variable store to be method create store. add variable store as invocation of createStore. export createStore for testing

--- fuel_splitter/src/store/store.ts
+++ fuel_splitter/src/store/store.ts
@@ -2,16 +2,30 @@ import { configureStore } from "@reduxjs/toolkit";
 import historySlice from "./historySlice";
 import fuelBill from "./GasTripSlice";
 
-const store = configureStore({
-  reducer: {
-    fuelHistory: historySlice.reducer,
-    fuelBill: fuelBill.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-});
+// const store = configureStore({
+//   reducer: {
+//     fuelHistory: historySlice.reducer,
+//     fuelBill: fuelBill.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),
+// });
+
+export const createStore = () =>
+  configureStore({
+    reducer: {
+      fuelHistory: historySlice.reducer,
+      fuelBill: fuelBill.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+  });
+
+const store = createStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 
