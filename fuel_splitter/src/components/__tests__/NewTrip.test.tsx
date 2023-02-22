fbe3249bc4df5d381b5b2a82b0bf0504fb311b59
Brendan Woo <woo.brendan@gmail.com>
2023-02-22 14:01:53 -0800

    add new test for confirming check boxes can be checked

--- fuel_splitter/src/components/__tests__/NewTrip.test.tsx
+++ fuel_splitter/src/components/__tests__/NewTrip.test.tsx
@@ -1,4 +1,5 @@
 import NewTrip from "../TripManage/New_trip/NewTrip";
+import TripCheckbox from "../TripManage/New_trip/TripCheckbox";
 import { render, fireEvent, screen } from "@testing-library/react";
 import { GasBill, FuelBill } from "../../model";
 import { Provider } from "react-redux";
@@ -11,4 +12,17 @@ const comp = (
   </Provider>
 );
 
-describe(NewTrip, () => {});
+describe(NewTrip, () => {
+  it("Should toggle checkbox", () => {
+    const handleClick = jest.fn();
+    const name = { name: "Brendan", value: false };
+    const { getByTestId } = render(
+      <TripCheckbox nameVal={name} onCheck={handleClick} />,
+    );
+
+    const checkbox = getByTestId("checkbox_Brendan");
+    fireEvent.click(checkbox);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
