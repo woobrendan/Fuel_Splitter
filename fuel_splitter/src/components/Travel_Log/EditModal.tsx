95400dd6adff580efc603ef3b76acc93449ff434
Brendan Woo <woo.brendan@gmail.com>
2023-02-27 12:29:43 -0800

    import interface TripDetails and pass tripDetails props. deconstruct same

--- fuel_splitter/src/components/Travel_Log/EditModal.tsx
+++ fuel_splitter/src/components/Travel_Log/EditModal.tsx
@@ -1,14 +1,18 @@
 import { Modal, Box } from "@mui/material";
+import "../../Styles/modal.scss";
+import { TripDetails } from "../../model";
 
 interface Props {
   show: boolean;
   handleToggle: () => void;
+  tripDetails: TripDetails;
 }
 
-const EditModal: React.FC<Props> = ({ show, handleToggle }) => {
+const EditModal: React.FC<Props> = ({ show, handleToggle, tripDetails }) => {
+  // render out data as inputs and create onchange to set and then return the tripDetails
   return (
     <Modal open={show} onClose={handleToggle}>
-      <Box>Seome Stuff</Box>
+      <Box id="edit_modal">Seome Stuff</Box>
     </Modal>
   );
 };
