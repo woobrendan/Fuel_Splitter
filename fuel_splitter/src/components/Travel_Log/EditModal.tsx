ab8ed2184f379564a2d4bb04b8f0fd5923b1f8cc
Brendan Woo <woo.brendan@gmail.com>
2023-02-27 12:06:04 -0800

    create interface props and add to component. deconstruct props and pass to modal. import box and render inside modal

--- fuel_splitter/src/components/Travel_Log/EditModal.tsx
+++ fuel_splitter/src/components/Travel_Log/EditModal.tsx
@@ -1,5 +1,16 @@
-const EditModal: React.FC = () => {
-  return <> </>;
+import { Modal, Box } from "@mui/material";
+
+interface Props {
+  show: boolean;
+  handleToggle: () => void;
+}
+
+const EditModal: React.FC<Props> = ({ show, handleToggle }) => {
+  return (
+    <Modal open={show} onClose={handleToggle}>
+      <Box>Seome Stuff</Box>
+    </Modal>
+  );
 };
 
 export default EditModal;
