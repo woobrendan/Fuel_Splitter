c7b8212acc40f24f23e7092991486063ae2abea3
Brendan Woo <woo.brendan@gmail.com>
2023-02-28 14:26:56 -0800

    create usestate modalTrip with TripDetails. move handleChange from parent into this component. and change values of inputs to be modalTrip.x

--- fuel_splitter/src/components/Travel_Log/EditModal.tsx
+++ fuel_splitter/src/components/Travel_Log/EditModal.tsx
@@ -1,39 +1,57 @@
-import { Modal, Box } from "@mui/material";
+import { Modal, Box, Button } from "@mui/material";
 import "../../Styles/modal.scss";
 import { TripDetails } from "../../model";
+import { useState } from "react";
 
 interface Props {
   show: boolean;
   handleToggle: () => void;
   tripDetails: TripDetails;
-  handleChange: (e: React.FormEvent) => void;
+  updateTrip: (val: TripDetails) => void;
 }
 
-const EditModal: React.FC<Props> = ({
-  show,
-  handleToggle,
-  tripDetails,
-  handleChange,
-}) => {
+const EditModal: React.FC<Props> = ({ show, handleToggle, tripDetails }) => {
   const { date, totalKM, description, involved } = tripDetails;
+  const [modalTrip, setModalTrip] = useState<TripDetails>({
+    date,
+    totalKM,
+    description,
+    involved,
+  });
+
+  const handleOnChange = (e: React.FormEvent) => {
+    const target = e.target as HTMLTextAreaElement;
+    const value =
+      target.name === "totalKM" ? Number(target.value) : target.value;
+    setModalTrip((prev) => ({
+      ...prev,
+      [target.name]: value,
+    }));
+  };
+
+  const onSubmit = () => {};
+
   // render out data as inputs and create onchange to set and then return the tripDetails
   return (
     <Modal open={show} onClose={handleToggle}>
       <Box id="edit_modal">
         <div>
           <input
-            value={date}
+            value={modalTrip.date}
             name="date"
             type="input"
-            onChange={(e) => handleChange(e)}
+            onChange={(e) => handleOnChange(e)}
           />
           <input
-            value={description}
+            value={modalTrip.description}
             name="description"
             type="input"
-            onChange={(e) => handleChange(e)}
+            onChange={(e) => handleOnChange(e)}
           />
         </div>
+        <Button variant="contained" color="success">
+          Update
+        </Button>
       </Box>
     </Modal>
   );
