3f484fda97311495eb065d3282665cdfa531d5d1
Brendan Woo <woo.brendan@gmail.com>
2023-02-28 12:49:09 -0800

    add handleChange to interface props. add two input components with values for descriptiopn and date

--- fuel_splitter/src/components/Travel_Log/EditModal.tsx
+++ fuel_splitter/src/components/Travel_Log/EditModal.tsx
@@ -6,13 +6,35 @@ interface Props {
   show: boolean;
   handleToggle: () => void;
   tripDetails: TripDetails;
+  handleChange: (e: React.FormEvent) => void;
 }
 
-const EditModal: React.FC<Props> = ({ show, handleToggle, tripDetails }) => {
+const EditModal: React.FC<Props> = ({
+  show,
+  handleToggle,
+  tripDetails,
+  handleChange,
+}) => {
+  const { date, totalKM, description, involved } = tripDetails;
   // render out data as inputs and create onchange to set and then return the tripDetails
   return (
     <Modal open={show} onClose={handleToggle}>
-      <Box id="edit_modal">Seome Stuff</Box>
+      <Box id="edit_modal">
+        <div>
+          <input
+            value={date}
+            name="date"
+            type="input"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            value={description}
+            name="description"
+            type="input"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+      </Box>
     </Modal>
   );
 };
