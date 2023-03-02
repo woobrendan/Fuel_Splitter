804c0e691d42e83b69a8e3532abc92b83b7ce5b4
Brendan Woo <woo.brendan@gmail.com>
2023-03-02 15:40:19 -0800

    add TripInfo and NameVal interface. add method getCheckValues

--- fuel_splitter/src/Models/tripModels.ts
+++ fuel_splitter/src/Models/tripModels.ts
@@ -1,4 +1,15 @@
-export const initialTripState = {
+export interface TripInfo {
+  isBrendanIn: boolean;
+  isLoryIn: boolean;
+  isDavidIn: boolean;
+  isParcoIn: boolean;
+  totalKM: number;
+  date: Date | null;
+  description: string;
+  _id?: string;
+}
+
+export const initialTripState: TripInfo = {
   isBrendanIn: false,
   isLoryIn: false,
   isDavidIn: false,
@@ -6,4 +17,18 @@ export const initialTripState = {
   totalKM: 0,
   date: new Date(),
   description: "",
+};
+
+export interface NameVal {
+  name: string;
+  value: boolean;
+}
+
+export const getCheckValues = (trip: TripInfo): NameVal[] => {
+  return [
+    { name: "Brendan", value: trip.isBrendanIn },
+    { name: "Lory", value: trip.isLoryIn },
+    { name: "David", value: trip.isDavidIn },
+    { name: "Parco", value: trip.isParcoIn },
+  ];
 };
