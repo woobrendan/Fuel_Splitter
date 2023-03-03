80535cbea5ca05a7281542bb90e8daf07b7d732d
Brendan Woo <woo.brendan@gmail.com>
2023-03-03 10:50:11 -0800

    add in interface props and deconstruct all. update all values and props to child components

--- fuel_splitter/src/components/TripManage/TripManage.tsx
+++ fuel_splitter/src/components/TripManage/TripManage.tsx
@@ -1,30 +1,49 @@
-const TripManager: React.FC = () => {
+import TripCheckList from "./New_trip/TripCheckList";
+import InputContainer from "./InputContainer";
+import { TripInfo } from "../../Models/tripModels";
+import { tripErrorHandle } from "../../Models/errorModels";
+
+interface Props {
+  trip: TripInfo;
+  onInputChange: (e: React.FormEvent) => void;
+  onCheck: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error: tripErrorHandle;
+  comp: string;
+}
+
+const TripManager: React.FC<Props> = ({
+  trip,
+  onInputChange,
+  onCheck,
+  error,
+  comp,
+}) => {
   return (
     <>
       <TripCheckList
         error={error.hasCheck}
-        tripInfo={tripInfo}
+        tripInfo={trip}
         onCheck={onCheck}
-        comp="newTrip"
+        comp={comp}
       />
       <section className="newTrip__input">
         <InputContainer
-          val={tripInfo.totalKM}
+          val={trip.totalKM}
           error={error.hasDistance}
           onInputChange={onInputChange}
           label="Total KM"
           name="totalKM"
           type="input"
-          comp="newTrip"
+          comp={comp}
         />
         <InputContainer
-          val={tripInfo.description}
+          val={trip.description}
           error={error.hasDescription}
           onInputChange={onInputChange}
           label="Trip Description"
           name="description"
           type="input"
-          comp="newTrip"
+          comp={comp}
         />
       </section>
     </>
