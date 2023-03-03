af7c63541b384b1c3f30ab74f04a8de57cf70f88
Brendan Woo <woo.brendan@gmail.com>
2023-03-03 10:34:27 -0800

    fix imports for formgroup tripcheckboxes and models. add interface props and deconstruct

--- fuel_splitter/src/components/TripManage/New_trip/TripCheckList.tsx
+++ fuel_splitter/src/components/TripManage/New_trip/TripCheckList.tsx
@@ -1,4 +1,15 @@
-const TripCheckList: React.FC = () => {
+import { FormGroup } from "@mui/material";
+import TripCheckbox from "./TripCheckbox";
+import { TripInfo, getCheckValues, NameVal } from "../../../Models/tripModels";
+import React from "react";
+
+interface Props {
+  tripInfo: TripInfo;
+  onCheck: (e: React.FormEvent) => void;
+  error: boolean;
+}
+
+const TripCheckList: React.FC<Props> = ({ tripInfo, onCheck, error }) => {
   return (
     <>
       <h2>Trip Participants</h2>
