b103cf668cd8cc182d4686bf59cd11c80a9ba2d9
Brendan Woo <woo.brendan@gmail.com>
2023-03-08 11:59:04 -0800

    add in new connection url for testing

--- express_server/config/config.ts
+++ express_server/config/config.ts
@@ -6,13 +6,15 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/`;
 
+const MONGO_TEST = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/fuel_test`;
+
 const SERVER_PORT = process.env.SERVER_PORT
   ? Number(process.env.SERVER_PORT)
   : 2020;
 
 export const config = {
   mongo: {
-    url: MONGO_URL,
+    url: MONGO_TEST,
   },
   server: {
     port: SERVER_PORT,
