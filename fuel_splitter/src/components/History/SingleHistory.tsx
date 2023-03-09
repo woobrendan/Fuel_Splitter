1fe8c74defb60ae042679e011212b2c95edfadae
Brendan Woo <woo.brendan@gmail.com>
2023-03-09 10:37:01 -0800

    add usestate for delete. add in buttoon for deleting history

--- fuel_splitter/src/components/History/SingleHistory.tsx
+++ fuel_splitter/src/components/History/SingleHistory.tsx
@@ -2,6 +2,8 @@ import { FuelBill } from "../../model";
 import TravelList from "../Travel_Log/TravelList";
 import TotalBreakdown from "./TotalBreakdown";
 import IndivBreakDown from "./IndivBreakDown";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 interface Props {
   bill: FuelBill;
@@ -9,8 +11,19 @@ interface Props {
 }
 
 const SingleHistory: React.FC<Props> = ({ bill, tripNum }) => {
+  const [toBeDeleted, setToBeDeleted] = useState(false);
+
   return (
     <section className="history__single">
+      <Button
+        variant="outlined"
+        color="error"
+        className="history_delete"
+        data-testid="modal_delete"
+        onClick={() => setToBeDeleted(!toBeDeleted)}
+      >
+        {toBeDeleted ? "Cancel" : "Delete Trip"}
+      </Button>
       <h2>Trip Summary #{tripNum}</h2>
       <div className="history__single__summary">
         <div className="history__single__breakdown">
