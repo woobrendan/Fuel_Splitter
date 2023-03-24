f785058687a22e7c87ddcc2d8e8b7c9b9fd9369a
Brendan Woo <woo.brendan@gmail.com>
2023-03-24 13:36:20 -0700

    fix routes, to not have words like get, update, delete or new. folow restful api rules

--- express_server/routes/tripLog.ts
+++ express_server/routes/tripLog.ts
@@ -3,11 +3,11 @@ import controller from "../controllers/tripLog_controller";
 
 const router = express.Router();
 
-router.post("/create", controller.createTripLog);
-router.get("/get/:tripId", controller.readTripLog);
-router.get("/get/", controller.readAll);
-router.patch("/update/:tripId", controller.udpateTripLog);
-router.delete("/delete/all/", controller.deleteAll);
-router.delete("/delete/:tripId", controller.deleteTripLog);
+router.post("/", controller.createTripLog);
+router.get("/", controller.readAll);
+router.get("/:tripId", controller.readTripLog);
+router.patch("/:tripId", controller.udpateTripLog);
+router.delete("/all", controller.deleteAll);
+router.delete("/:tripId", controller.deleteTripLog);
 
 export = router;
